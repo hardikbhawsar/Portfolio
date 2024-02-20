b48fad919f44019a7b8aafea26a86aa68a35a31b
Hardik Bhawsar <hardik.13bhawsar@gmail.com>
2024-02-19 23:28:53 -0800

    Implement Navbar.js Logo

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,68 +1,99 @@
-import { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap"
-import { ArrowRightCircle, Balloon, CaretRightFill, ChevronDoubleRight, EmojiSmile, FastForward } from "react-bootstrap-icons"
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
-// import 'animate.css';
-// import TrackVisibility from 'react-on-screen';
+import { ArrowRightCircle, FastForward } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+import { HashLink } from "react-router-hash-link";
 
 export const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const [index, setIndex] = useState(1);
-    const toRotate = ["Web Developer", "Full Stack Developer", "IOS App Developer", "Back-End Developer"];
-    const period = 1000;
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(200 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "FUll Stack Developer", "Backend Developer", "Software Developer" ];
+  const period = 1000;
 
-    useEffect(() => {
-        let ticker = setInterval(() => {
-            tick();
-        }, delta);
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-        return () => { clearInterval(ticker) };
-    }, [text])
+    return () => { clearInterval(ticker) };
+  }, [text])
 
-    const tick = () => {
-        let i = loopNum % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-        setText(updatedText);
+    setText(updatedText);
 
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2);
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setIndex(prevIndex => prevIndex - 1);
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setIndex(1);
-            setDelta(200);
-        } else {
-            setIndex(prevIndex => prevIndex + 1);
-        }
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
     }
 
-    return (
-        <section className="banner" id="home">
-            <Container>
-                <Row className="aligh-items-center">
-                    <Col xs={5} md={10} xl={7}>
-                        <span className="tagline">Welcome to my Portfolio</span>
-                        <h1>{"Hi, I'm Hardik!"}</h1>
-                        <h3><FastForward size={25} />{' '}<span className="wrap">{text}</span>{' '}</h3>
-                        <p>My name is Hardik Bhawsar. I am passionate about web development, LLM, machine learning, IOS app development and data structure. </p>
-                        <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
-                    </Col>
-                    <Col xs={5} md={6} xl={5}>
-                        <img className="backimg" src={headerImg} alt="Header Img" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    )
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(300);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                         <h1>{"Hi, I'm Hardik!"}</h1>
+                         <h3><FastForward size={25} />{' '}<span className="wrap">{text}</span>{' '}</h3>
+                         <p>I am a skilled and passionate full stack and backend developer with experience in creating visually appealing and user-friendly web applications.</p>
+                         
+                            <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+                         
+              </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__zoomIn" : ""}>
+                  <img src={headerImg} alt="Header Img"/>
+                </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
 }
+    // return (
+    //     <section className="banner" id="home">
+    //         <Container>
+    //             <Row className="aligh-items-center">
+    //                 <Col xs={5} md={10} xl={7}>
+    //                     <span className="tagline">Welcome to my Portfolio</span>
+    //                     <h1>{"Hi, I'm Hardik!"}</h1>
+    //                     <h3><FastForward size={25} />{' '}<span className="wrap">{text}</span>{' '}</h3>
+    //                     <p>My name is Hardik Bhawsar. I am passionate about web development, LLM, machine learning, IOS app development and data structure. </p>
+    //                     <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25} /></button>
+    //                 </Col>
+    //                 <Col xs={5} md={6} xl={5}>
+    //                     <img className="backimg" src={headerImg} alt="Header Img" />
+    //                 </Col>
+    //             </Row>
+    //         </Container>
+    //     </section>
+    // )
---
+++ src/components/Contact.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Contact = () => {
+  const form = useRef();
+  const formInitialDetails = {
+    user_name: '',
+    user_email: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+      setFormDetails({
+        ...formDetails,
+        [category]: value
+      })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await emailjs
+          .sendForm('service_q7m5qmb', 'template_8te4oxe', form.current, {
+          publicKey: 'lrn4ORZspFk6oKt5b',
+          });
+    setButtonText("Send");
+    setFormDetails(formInitialDetails);
+    if (response.status == 200 || response.text == "OK") {
+      setStatus({ succes: true, message: 'Message sent successfully'});
+    } else {
+      setStatus({ succes: false, message: 'Something went wrong, please try again later.'});
+    }
+  };
+
+  return (
+    <section className="contact" id="connect">
+      <Container>
+        <Row className="align-items-center">
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+              }
+            </TrackVisibility>
+          </Col>
+          <Col size={12} md={6}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <h2>Get In Touch</h2>
+                <form ref={form} onSubmit={handleSubmit}>
+                  <Row>
+                    <Col size={12} sm={12} className="px-1">
+                      <input type="text" value={formDetails.user_name} placeholder="Name" onChange={(e) => onFormUpdate('user_name', e.target.value)} />
+                    </Col>
+                    {/* <Col size={12} sm={6} className="px-1">
+                      <input type="text" value={formDetails.lasttName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
+                    </Col> */}
+                    <Col size={12} sm={12} className="px-1">
+                      <input type="user_email" value={formDetails.user_email} placeholder="Email Address" onChange={(e) => onFormUpdate('user_email', e.target.value)} />
+                    </Col>
+                    {/* <Col size={12} sm={6} className="px-1">
+                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                    </Col> */}
+                    <Col size={12} className="px-1">
+                      <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
+                      <button type="submit">Submit</button>
+                    </Col>
+                    {/* {
+                      status.message &&
+                      <Col>
+                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
+                      </Col>
+                    } */}
+                  </Row>
+                </form>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
